1843fd51f874e639747c379ec1230bb785b2a430
Thaitan1 <143531122+Thaitan1@users.noreply.github.com>
2025-11-11 19:54:02 +0100

    Add files via upload

---
+++ ChatExamination/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ChatExamination;
+using SocketIOClient;
+
+class Message
+{
+    public string Text { get; set; }
+    public string Sender { get; set; }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Pick a username:");
+        var userName = Console.ReadLine();
+
+        while (userName == "")
+        {
+            Console.WriteLine("Please enter username:");
+            userName = Console.ReadLine();
+        }
+
+        Console.WriteLine($"Your username is: {userName}");
+    
+        await SocketManager.Connect();
+
+        while (true)
+        {
+
+        }
+    }
+}
+        
+    
---
+++ ChatExamination/SocketManager.cs
@@ -0,0 +1,50 @@
+﻿namespace ChatExamination;
+using SocketIOClient;
+
+public class SocketManager
+{
+    private static SocketIO _client;
+    private static readonly string Path = "/sys25d";
+    public static List<string> messages;
+
+    static SocketManager()
+    {
+        messages = [];
+    }
+
+    public static async Task Connect()
+    {
+        _client = new SocketIO("wss://api.leetcode.se", new SocketIOOptions
+        {
+            Path = Path
+        });
+
+        _client.On("message", response =>
+        {
+            string receivedMessage = response.GetValue<string>();
+            Console.WriteLine($"Received message: {receivedMessage}");
+        });
+
+        _client.OnConnected += (sender, args) =>
+        {
+            Console.WriteLine("Connected");
+        };
+        
+        _client.OnDisconnected += (sender, args) =>
+        {
+            Console.WriteLine("Disconnected");
+        };
+        
+        await _client.ConnectAsync();
+
+        await Task.Delay(2000);
+        
+        Console.WriteLine($"Connected: {_client.Connected}");
+    }
+
+    public static async Task SendMessage(string message)
+    {
+        await  _client.EmitAsync("message", message);
+        Console.WriteLine($"You said: {message}");
+    }
+}
